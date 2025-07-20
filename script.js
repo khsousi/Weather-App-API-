523b8d86ea537c4f5876407898dde01746a35d98
khaled <khaledwns20@gmail.com>
2025-07-20 09:32:25 +0300

    Creating Weather App

--- script.js
+++ script.js
@@ -283,22 +283,133 @@
   // This How To Get The Same API with async/await 
 
 
-async function fetchData() {
-  try {
-    const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-    if (!response.ok){
-      throw new Error("Couldn't Fetch Data")
-    }
-    const data = await response.json()
-    const pokemonImage = data.sprites.front_default;
-    const imgElement = document.getElementById("pokemonImage")
+// async function fetchData() {
+//   try {
+//     const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+//     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+//     if (!response.ok){
+//       throw new Error("Couldn't Fetch Data")
+//     }
+//     const data = await response.json()
+//     const pokemonImage = data.sprites.front_default;
+//     const imgElement = document.getElementById("pokemonImage")
     
-    imgElement.src = pokemonImage
-    imgElement.style.display = "block"
-  } catch (error) {
-    console.error(error)
+//     imgElement.src = pokemonImage
+//     imgElement.style.display = "block"
+//   } catch (error) {
+//     console.error(error)
+//   }
+// }
+
+// fetchData()
+
+
+// Weather App 
+
+const weatherForm = document.querySelector(".weatherForm")
+const cityInput = document.querySelector(".cityInput")
+const card = document.querySelector(".card")
+const apiKey = "b0a22dfd938dded1a5fbe5def4591f42"
+
+
+weatherForm.addEventListener("submit" , async event => {
+  event.preventDefault()
+  const city = cityInput.value
+
+  if (city) {
+    try {
+      const weatherData = await getWeatherData(city)
+      displayWeatherInfo(weatherData)
+    } catch (error) {
+      console.log(error)
+      displayError(error)
+    }
+    
+  }else {
+    displayError("Please Enter A City")
+  }
+})
+
+async function getWeatherData(city) {
+  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+  const response = await fetch(apiUrl)
+
+  if (!response.ok) {
+    throw new Error("Could Not Fetch Data");
+  }
+  return await response.json()
+}
+
+function displayWeatherInfo(data) {
+  
+  const { name: city ,
+          main: {temp , humidity},
+          weather: [{description , id}]} = data 
+
+  card.textContent = ""
+  card.style.display  = "flex"
+
+  const cityDisplay = document.createElement("h1")
+  const tempDisplay = document.createElement("p")
+  const humidityDisplay = document.createElement("p")
+  const descDisplay = document.createElement("p")
+  const weatherEmoji = document.createElement("p")
+
+
+  
+  cityDisplay.textContent = city
+  tempDisplay.textContent = `${(temp - 273.15).toFixed(2)}°C`
+  humidityDisplay.textContent = `Humidity : ${humidity} %`
+  descDisplay.textContent = description
+  weatherEmoji.textContent = getWeatherEmoji(id)
+
+
+  cityDisplay.classList.add("cityDisplay")
+  tempDisplay.classList.add("tempDisplay")
+  humidityDisplay.classList.add("humidityDisplay")
+  descDisplay.classList.add("descDisplay")
+  weatherEmoji.classList.add("weatherEmoji")
+
+  
+  card.appendChild(cityDisplay)
+  card.appendChild(tempDisplay)
+  card.appendChild(humidityDisplay)
+  card.appendChild(descDisplay)
+  card.appendChild(weatherEmoji)
+
+  cityInput.value = ""
+} 
+
+function getWeatherEmoji(weatherId) {
+  
+  switch (true) {
+    case (weatherId >= 200 && weatherId < 300):
+      return "⛈"
+      case (weatherId >= 300 && weatherId < 400):
+      return "🌧"
+      case (weatherId >= 500 && weatherId < 600):
+      return "🌧"
+      case (weatherId >= 600 && weatherId < 700):
+      return "❄"
+      case (weatherId >= 700 && weatherId < 800):
+      return "🎿"
+      case (weatherId === 800):
+      return "☀"
+      case (weatherId >= 801 && weatherId < 810):
+      return "☁"
+    default:
+      return "❓"
   }
 }
 
-fetchData()
+function displayError(message) {
+  const errorDisplay = document.createElement("p")
+  errorDisplay.textContent = message
+  errorDisplay.classList.add("errorDisplay")
+
+  card.textContent = ""
+  card.style.display = "flex"
+  card.appendChild(errorDisplay)
+}
+
