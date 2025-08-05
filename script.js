1d25abaac1aa2af4eff17695a91ac141a27ba9db
Khaled Sousi <107036069+khsousi@users.noreply.github.com>
2025-08-05 10:34:42 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const cityInput = document.querySelector(".cityInput")
 const card = document.querySelector(".card")
 const apiKey = "b0a22dfd938dded1a5fbe5def4591f42"
  
- 
+  
 weatherForm.addEventListener("submit" , async event => {
   event.preventDefault()
   const city = cityInput.value
