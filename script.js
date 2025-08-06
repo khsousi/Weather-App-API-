aa8e15df863017029b9988aee6e69b282445a9e3
Khaled Sousi <107036069+khsousi@users.noreply.github.com>
2025-08-06 12:39:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
     
 // Weather App 
 
+
 const weatherForm = document.querySelector(".weatherForm")
 const cityInput = document.querySelector(".cityInput")
 const card = document.querySelector(".card")
